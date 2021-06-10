f2168c2eb32bf544620aad0cba1b50dc3504e745
tasszz2k <anhdt118@ghtk.co>
2021-06-10 11:07:49 +0700

    add hashcode & equals, fix object as key Hashmap

--- src/model/Product.java
+++ src/model/Product.java
@@ -52,6 +52,25 @@ public class Product {
                 '}';
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+
+        Product other = (Product) obj;
+        return id.equals(other.id);
+    }
 
 }
